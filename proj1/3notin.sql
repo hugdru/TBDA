4c14300b25ca16b77ed33b76fd9ba219f5f885e8
Hugo Drumond <hugdru@gmail.com>
2017-03-22 19:27:13 +0000

    comentarios

--- proj1/3notin.sql
+++ proj1/3notin.sql
@@ -1,17 +1,20 @@
+-- x
 SELECT NOME
 FROM XDISCIPLINA
-WHERE disciplina_id NOT IN(
-	SELECT disciplina_id
-	FROM xrespostas)
+WHERE disciplina_id NOT IN (
+  SELECT disciplina_id
+  FROM xrespostas);
 
+-- y
 SELECT nome
 FROM ydisciplina
-WHERE disciplina_id NOT IN(
-	SELECT disciplina_id
-	FROM yrespostas)
+WHERE disciplina_id NOT IN (
+  SELECT disciplina_id
+  FROM yrespostas);
 
+-- z
 SELECT nome
 FROM zdisciplina
-WHERE disciplina_id NOT IN(
-	SELECT disciplina_id
-	FROM zrespostas)
+WHERE disciplina_id NOT IN (
+  SELECT disciplina_id
+  FROM zrespostas);
