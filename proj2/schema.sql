b944069adff2b42e7a13993f74269f39a6fc8bb6
Rui <ruidcscg@sapo.pt>
2017-04-30 17:42:39 +0100

    faltam as tables

--- proj2/schema.sql
+++ proj2/schema.sql
@@ -15,6 +15,7 @@ CREATE TYPE item_tab_refs AS TABLE OF item_refs;
 ALTER TYPE item_t ADD ATTRIBUTE (related_itens item_tab_refs) CASCADE;
 
 
+
 --------------------------------------------------------- SECTION
 CREATE TYPE section_t AS OBJECT(
 	id INTEGER,
@@ -25,6 +26,7 @@ CREATE TYPE section_t AS OBJECT(
 ALTER TYPE item_t ADD ATTRIBUTE (section REF section_t) CASCADE;
 
 
+
 --------------------------------------------------------- IMAGE
 CREATE TYPE image_t AS OBJECT(
 	id INTEGER,
@@ -33,7 +35,7 @@ CREATE TYPE image_t AS OBJECT(
 	file_size NUMBER,
 	url NVARCHAR2(2083),
 	itens_image item_tab_refs
-);
+) NOT FINAL;
 
 CREATE TYPE image_refs AS OBJECT(
 	image REF image_t
@@ -44,6 +46,28 @@ CREATE TYPE image_tab_refs AS TABLE OF image_refs;
 ALTER TYPE item_t ADD ATTRIBUTE (images_item image_tab_refs) CASCADE;
 
 
+
+--------------------------------------------------------- CARTOON
+CREATE TYPE cartoon_t UNDER image_t();
+
+
+
+--------------------------------------------------------- INFOGRAM
+CREATE TYPE infogram_t UNDER image_t();
+
+
+
+--------------------------------------------------------- PHOTO
+CREATE TYPE photo_t UNDER image_t();
+
+CREATE TYPE photo_refs AS OBJECT(
+	photo REF photo_t
+);
+
+CREATE TYPE photo_tab_refs AS TABLE OF photo_refs;
+
+
+
 --------------------------------------------------------- JOURNALIST
 CREATE TYPE journalist_t AS OBJECT(
 	id INTEGER,
@@ -51,6 +75,12 @@ CREATE TYPE journalist_t AS OBJECT(
 	professional_id NCHAR(30),
 	images_journalist image_tab_refs
 );
+
+CREATE TYPE journalist_refs AS OBJECT(
+	journalist REF journalist_t
+);
+
+CREATE TYPE journalist_tab_refs AS TABLE OF journalist_refs;
 
 ALTER TYPE image_t ADD ATTRIBUTE (journalist REF journalist_t) CASCADE;
 
@@ -75,43 +105,90 @@ CREATE TYPE video_tab_refs AS TABLE OF video_refs;
 ALTER TYPE item_t ADD ATTRIBUTE (videos video_tab_refs) CASCADE;
 
 
+
 --------------------------------------------------------- PLACE
 CREATE TYPE place_t AS OBJECT(
 	id INTEGER,
 	city NVARCHAR2(300),
 	name NVARCHAR2(500),
 	videos_place video_tab_refs
+	photos photo_tab_refs
 );
+
+CREATE TYPE place_refs AS OBJECT(
+	place REF place_t
+);
+
+CREATE TYPE place_tab_refs AS TABLE OF place_refs;
 
 ALTER TYPE video_t ADD ATTRIBUTE (place REF place_t) CASCADE;
 
+ALTER TYPE photo_t ADD ATTRIBUTE (place REF place_t) CASCADE;
 
 
------------------------------------------------ USER
-/*CREATE TYPE user_t AS OBJECT(
+
+--------------------------------------------------------- CATEGORY
+CREATE TYPE category_t AS OBJECT(
+	id INTEGER,
+	name NVARCHAR2(200),
+	journalist journalist_tab_refs
+);
+
+ALTER TYPE journalist_t ADD ATTRIBUTE (category REF category_t) CASCADE;
+
+
+
+--------------------------------------------------------- USER
+CREATE TYPE user_t AS OBJECT(
 	id INTEGER,
 	username NVARCHAR2(100),
 	joining_date DATE,
 	paid_date DATE,
-	item_counter INTEGER,
-	country REF country_t
+	item_counter INTEGER
 );
 
-CREATE TYPE user_tab AS TABLE OF user_t;
+CREATE TYPE user_refs AS OBJECT(
+	user REF user_t
+);
+
+CREATE TYPE user_tab_refs AS TABLE OF user_refs;
 
 
------------------------------------------------ COUNTRY
+
+--------------------------------------------------------- ITEM_USER
+CREATE TYPE item_user_t AS OBJECT(
+	downloaded_item REF item_t,
+	user REF user_t,
+	download_date DATE
+);
+
+CREATE TYPE item_user_refs AS OBJECT(
+	item_user REF item_user_t
+);
+
+CREATE TYPE item_user_tab_refs AS TABLE OF item_user_refs;
+
+ALTER TYPE item_t ADD ATTRIBUTE (users item_user_tab_refs) CASCADE;
+
+ALTER TYPE user_t ADD ATTRIBUTE (itens item_user_tab_refs) CASCADE;
+
+
+
+--------------------------------------------------------- COUNTRY
 CREATE TYPE country_t AS OBJECT(
 	id INTEGER,
 	name NVARCHAR2(100),
+	places place_tab_refs,
+	journalists journalist_tab_refs,
+	users user_tab_refs
 );
 
-CREATE TYPE country_tab AS TABLE OF country_t;
+ALTER TYPE place_t ADD ATTRIBUTE (country REF country_t) CASCADE;
 
-CREATE TABLE country of country_t
-	NESTED TABLE users STORE AS users_nt;
+ALTER TYPE journalist_t ADD ATTRIBUTE (country REF country_t) CASCADE;
 
-*/
+ALTER TYPE user_t ADD ATTRIBUTE (country REF country_t) CASCADE;
+
 
 
 
