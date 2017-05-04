32e13421adc0e6d7dc7debe9cefa160b804d29b8
Hugo Drumond <hugdru@gmail.com>
2017-05-04 23:23:22 +0100

    tex ready for csv with query results and sql files

--- proj2/d.sql
+++ proj2/d.sql
@@ -1,8 +1,9 @@
-SELECT value(p).title as image_title
-FROM TABLE (SELECT v.downloaded_item.images
-            FROM TABLE (SELECT c.downloaded_items
-                        FROM client_table c
-                        WHERE c.username = 'lugubrious46') v
-            WHERE v.downloaded_item.published_date = TO_TIMESTAMP('2017-05-01 23:55:00', 'YYYY-MM-DD HH24:MI:SS')) p;
---             WHERE v.download_date = TO_TIMESTAMP('2017-05-03 10:35:00', 'YYYY-MM-DD HH24:MI:SS')) p;
-
+SELECT value(p).title AS image_title
+FROM TABLE (
+     SELECT v.downloaded_item.images
+     FROM TABLE (
+          SELECT c.downloaded_items
+          FROM client_table c
+          WHERE c.username = 'lugubrious46') v
+     WHERE v.downloaded_item.published_date = TO_TIMESTAMP('2017-05-01 23:55:00', 'YYYY-MM-DD HH24:MI:SS')) p;
+--      WHERE v.download_date = TO_TIMESTAMP('2017-05-03 10:35:00', 'YYYY-MM-DD HH24:MI:SS')) p;
