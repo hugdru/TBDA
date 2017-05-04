e109ae8331655bca53145a4d4f6e3392f4df3ebf
Hugo Drumond <hugo.drumond@fe.up.pt>
2017-05-04 19:22:53 +0100

    timestamp in d.sql

--- proj2/d.sql
+++ proj2/d.sql
@@ -1,5 +1,7 @@
 -- d)
 SELECT value(p).title
-FROM table(SELECT v.downloaded_item.images
-           FROM table(select c.downloaded_items from client_table c where c.username='lugubrious46') v
-           WHERE v.download_date='17.05.03 10:35:00') p;
+FROM TABLE (SELECT v.downloaded_item.images
+            FROM TABLE (SELECT c.downloaded_items
+                        FROM client_table c
+                        WHERE c.username = 'lugubrious46') v
+            WHERE v.download_date = TO_TIMESTAMP('17.05.03 10:35:00', 'YYYY-MM-DD HH24:MI:SS')) p;
