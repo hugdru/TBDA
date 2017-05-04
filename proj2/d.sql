cb59eb9a7ea79d336989f204cc1a8ff58fafafe8
Hugo Drumond <hugdru@gmail.com>
2017-05-04 21:52:15 +0100

    fixed data in d.sql

--- proj2/d.sql
+++ proj2/d.sql
@@ -1,7 +1,9 @@
 -- d)
-SELECT value(p).title
+SELECT value(p).title as image_title
 FROM TABLE (SELECT v.downloaded_item.images
             FROM TABLE (SELECT c.downloaded_items
                         FROM client_table c
                         WHERE c.username = 'lugubrious46') v
-            WHERE v.download_date = TO_TIMESTAMP('17.05.03 10:35:00', 'YYYY-MM-DD HH24:MI:SS')) p;
+            WHERE v.downloaded_item.published_date = TO_TIMESTAMP('2017-05-01 23:55:00', 'YYYY-MM-DD HH24:MI:SS')) p;
+--             WHERE v.download_date = TO_TIMESTAMP('2017-05-03 10:35:00', 'YYYY-MM-DD HH24:MI:SS')) p;
+
