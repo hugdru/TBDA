a939997cf061f206c21844049d33f065b97691ce
Hugo Drumond <hugdru@gmail.com>
2017-05-05 01:31:51 +0100

    improved d.sql code

--- proj2/d.sql
+++ proj2/d.sql
@@ -1,9 +1,8 @@
-SELECT value(p).title AS image_title
+SELECT value(q).title AS image_title
 FROM TABLE (
-     SELECT v.downloaded_item.images
-     FROM TABLE (
-          SELECT c.downloaded_items
-          FROM client_table c
-          WHERE c.username = 'lugubrious46') v
-     WHERE v.downloaded_item.published_date = TO_TIMESTAMP('2017-05-01 23:55:00', 'YYYY-MM-DD HH24:MI:SS')) p;
---      WHERE v.download_date = TO_TIMESTAMP('2017-05-03 10:35:00', 'YYYY-MM-DD HH24:MI:SS')) p;
+     SELECT di.downloaded_item.images AS images
+     FROM client_table c, TABLE (c.DOWNLOADED_ITEMS) di
+     WHERE c.username = 'lugubrious46' AND
+           di.downloaded_item.published_date = TO_TIMESTAMP('2017-05-01 23:55:00', 'YYYY-MM-DD HH24:MI:SS')) q;
+--      WHERE c.username = 'lugubrious46' AND
+--            di.download_date = TO_TIMESTAMP('2017-05-03 10:35:00', 'YYYY-MM-DD HH24:MI:SS')) q;
