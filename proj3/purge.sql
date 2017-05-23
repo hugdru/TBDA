f402d36f0dad7097b0b2ce231bd2e473a27f19f4
Hugo Drumond <hugdru@gmail.com>
2017-05-23 20:42:45 +0100

    tables creation and addition of primary keys and foreign keys

---
+++ proj3/purge.sql
@@ -0,0 +1,54 @@
+BEGIN
+  FOR cur_rec IN (SELECT
+                    object_name,
+                    object_type
+                  FROM user_objects
+                  WHERE object_type IN
+                        ('TABLE',
+                         'VIEW',
+                         'PACKAGE',
+                         'PROCEDURE',
+                         'FUNCTION',
+                         'SEQUENCE'
+                        ))
+  LOOP
+    BEGIN
+      IF cur_rec.object_type = 'TABLE'
+      THEN
+        EXECUTE IMMEDIATE 'DROP '
+                          || cur_rec.object_type
+                          || ' "'
+                          || cur_rec.object_name
+                          || '" CASCADE CONSTRAINTS';
+      ELSE
+        EXECUTE IMMEDIATE 'DROP '
+                          || cur_rec.object_type
+                          || ' "'
+                          || cur_rec.object_name
+                          || '"';
+      END IF;
+      EXCEPTION
+      WHEN OTHERS
+      THEN
+      DBMS_OUTPUT.put_line('FAILED: DROP '
+                           || cur_rec.object_type
+                           || ' "'
+                           || cur_rec.object_name
+                           || '"'
+      );
+    END;
+  END LOOP;
+
+  FOR uo IN (SELECT *
+             FROM user_objects
+             WHERE object_type = 'TYPE') LOOP
+    BEGIN
+      EXECUTE IMMEDIATE ('DROP ' || uo.object_type || ' ' || uo.object_name || ' FORCE');
+      EXCEPTION
+      WHEN OTHERS THEN
+      CONTINUE;
+    END;
+  END LOOP;
+END;
+
+PURGE RECYCLEBIN;
